e9283b98d381dcfa59610b8810be090e1b519df2
Ezradestaw <ezradestaw@gmail.com>
2026-05-17 13:14:21 +0300

    Selection class update

--- Selection.java
+++ Selection.java
@@ -1,5 +1,12 @@
 package first_package;
 
 public class Selection {
+	public static void main(String[] args) {
+		double grade = 90;
+		if (grade>=60)
+			System.out.println("Passed");
+		else
+			System.out.println("Failed");
+			}
 
 }
