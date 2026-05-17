238f74dae74d22ac54bdc397fbadfc22c733cddd
Ezradestaw <ezradestaw@gmail.com>
2026-05-17 14:21:44 +0300

    second try

---
+++ Try1.java
@@ -0,0 +1,28 @@
+package first_package;
+
+public class Try1 {
+	public static void main(String[] args) {
+		//System.out.println("ezra the great");
+
+		if (true) {
+			System.out.println("ezra the great");
+//			System.out.println("ezra destaw the great");
+			;}
+		else
+			System.out.println("ezra destw mekonnen thw great");
+		if (4>3) {;;;;System.out.println("ezra the \n great");}
+//		java.util.Scanner input =  new java.util.Scanner(System.in);
+//		System.out.print("enter yor full name up to your father's grandfather");
+//		String name = input.nextLine();
+		System.out.println("ezra destaw mekonnen\\\\\\\\\\ndestw");
+		{;}
+		if (false);
+		else 
+			System.out.println("ezra is the great");
+		
+			
+		
+	}
+	
+
+}
